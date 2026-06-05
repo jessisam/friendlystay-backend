33f60823a25af0415a00c4fdc28ffcf1e23c2e26
jessisamk <jessik@karunya.edu.in>
2026-06-05 14:53:22 +0530

    Fix CORS for PUT and DELETE methods

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const adminRoutes = require("./routes/admin"); // NEW
 const pool = require("./db");
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
